43d95ac5b17e2e3e221308689751ed0bbb5f18f8
Ukranian.Falcon@gmail.com <Ukrainian.Falcon@gmail.com>
2024-03-10 21:07:16 +0200

    From laptop. Homework 2_2/5

---
+++ jan_2024_JS-master/Homeworks/HW_1/homework_1.js
@@ -0,0 +1,70 @@
+// -----------------------------1---------------------------
+
+// let x = "hello";
+// console.log(x);
+//
+// let y = "owu";
+// console.log(y);
+//
+// let a = "com";
+// console.log(a);
+//
+// let b = "ua";
+// console.log(b);
+//
+// let c = 1;
+// console.log(c);
+//
+// let d = 10;
+// console.log(d);
+//
+// let e = -999;
+// console.log(e);
+//
+// let f = 123;
+// console.log(f);
+//
+// const PI = 3.14;
+// console.log(PI);
+//
+// let g = 3.14;
+// console.log(g);
+//
+// let h = 2.7;
+// console.log(h);
+//
+// let i = 16;
+// console.log(i);
+//
+// let j = 10;
+// console.log("10" !== 10);
+//
+// let k = "abcdefg";
+// console.log("abcdefg" == 10);
+
+// -----------------------------2---------------------------
+
+// let firstName = "Mariia";
+// let middleName = "Mykhailivna";
+// let lastName = "Tomliak";
+// let person = `${lastName} ${firstName} ${middleName}`;
+// console.log(person);
+
+// -----------------------------3---------------------------
+
+// let a = 100;
+// console.log(typeof a, a);
+//
+// let b = '100';
+// console.log(typeof b, b);
+//
+// let c = true;
+// console.log(typeof c, c);
+
+// -----------------------------4---------------------------
+
+let fullName = prompt("enter your full name");
+console.log(fullName);
+let age = prompt("enter your age");
+console.log(age);
+
---
+++ jan_2024_JS-master/Homeworks/HW_2/homework_2.js
@@ -0,0 +1,151 @@
+<!--Масиви та об'єкти:-->
+// ---------------------------------------1.1---------------------------------------
+
+// let NamesAndNumbers= ["zlata", 5, "mariia", 6, "victoriia", 9, "nina", 4, "kristina", 8];
+// console.log(NamesAndNumbers.length);
+// console.log(NamesAndNumbers [0]);
+// console.log(NamesAndNumbers [1]);
+// console.log(NamesAndNumbers [2]);
+// console.log(NamesAndNumbers [3]);
+// console.log(NamesAndNumbers [4]);
+// console.log(NamesAndNumbers [5]);
+// console.log(NamesAndNumbers [6]);
+// console.log(NamesAndNumbers [7]);
+// console.log(NamesAndNumbers [8]);
+// console.log(NamesAndNumbers [9]);
+
+// ---------------------------------------1.2---------------------------------------
+
+// let books = {
+//     title: "The Wolf in th eWatchtower",
+//     pageCount: 447,
+//     genre: "fantasy"
+// }
+// console.log(books);
+
+// ---------------------------------------1.3---------------------------------------
+
+// let firstBook = {title: "Holly", pageCount: 464, genre: "horror",
+//     authors:["Stephen King", 76]};
+// let secondBook = {title: "Harry Potter and the Philosopher's Stone", pageCount: 368, genre: "fantasy",
+//     authors:["Joanne Rowling", 58]};
+// let thirdBook = {title: "Fourth Wing", pageCount: 512, genre: "romantic fantasy",
+//     authors:["Rebecca Yarros", 42]};
+// console.log(firstBook,secondBook, thirdBook);
+
+// ---------------------------------------1.4---------------------------------------
+
+// let users = [
+//     {name: "Alex", username: "Drakula", password: "qwerty"},
+//     {name: "Vasya", username: "Pampushka", password: "qwerty123"},
+//     {name: "Pasha", username: "Subaber", password: 01011990},
+//     {name: "Inna", username: "Koza123", password: "luckyGirl"},
+//     {name: "Dasha", username: "Romashka", password: "1990qwerty"},
+//     {name: "Kostia", username: "GoldBoy", password: "user123"},
+//     {name: "Lina", username: "Guide", password: "guide1989"},
+//     {name: "Andrii", username: "Proffy", password: "poliglot09"},
+//     {name: "Rostik", username: "StreetMan", password: "mypassword"},
+//     {name: "Olia", username: "Doctor", password: "strongpassword"}]
+//  console.log(users.length);
+//  console.log(users);
+//  console.log(users[0].password);
+//  console.log(users[1].password);
+//  console.log(users[2].password);
+//  console.log(users[3].password);
+//  console.log(users[4].password);
+//  console.log(users[5].password);
+//  console.log(users[6].password);
+//  console.log(users[7].password);
+//  console.log(users[8].password);
+//  console.log(users[9].password);
+
+<!--Логічні розгалуження:-->
+// ---------------------------------------2.1---------------------------------------
+
+// let x = 24;
+// if (x !== 0) {
+//     console.log("Вірно");
+// } else {
+//     console.log("Невірно");
+// }
+//
+// //перевіряємо скрипт на "а", що дорівнює 0
+// let a = 0;
+// if (a !== 0) {
+//     console.log("Вірно");
+// } else {
+//     console.log("Невірно");
+// }
+
+//перевіряємо скрипт на "а", що дорівнює 1
+// let a = 1;
+// if (a !== 0) {
+//     console.log("Вірно");
+// } else {
+//     console.log("Невірно");
+// }
+
+//перевіряємо скрипт на "а", що дорівнює -3
+// let a = -3;
+// if (a !== 0) {
+//     console.log("Вірно");
+// } else {
+//     console.log("Невірно");
+// }
+
+// ---------------------------------------2.2---------------------------------------
+
+// let time = 17; // моє число 17 // console.log = document.write("<h2>Перша/друга/третя/четверта четверть години<h2/>")
+// if (0 <= time && time < 15) {
+//     console.log("Перша четверть години");
+// } else if (15 <= time && time < 30) {
+//     console.log("Друга четверть години");
+// } else if (30 <= time && time < 45) {
+//     console.log("Третя четверть години");
+// } else if (45 <= time && time < 60) {
+//     console.log("Четверта четверть години");
+// } else {
+//     console.log("Число не в діапазоні від 0 до 59");
+// }
+
+// ---------------------------------------2.3---------------------------------------
+
+// let day = 28 // console.log = document.write("<h2>Перша/друга/третя декада місяця<h2/>")
+// if(0 <= day && day < 10) {
+//     console.log("Перша декада місяця");
+// } else if(10 <= day && day < 20) {
+//     console.log("Друга декада місяця");
+// } else if(20 <= day && day <31) {
+//     console.log("Третя декада місяця");
+// } else {
+//     console.log("Число не в діапазоні від 0 до 31");
+// }
+
+// ---------------------------------------2.4---------------------------------------
+
+let schedul = {day: 5, task: 'do it'};
+    switch (schedul.day){
+        case 1:
+            console.log('Monday:learn JS');
+            break;
+        case 2:
+            console.log('Tuesday: learn English');
+            break;
+        case 3:
+           console.log('Wednesday: chatting with mentor');
+           break;
+        case 4:
+            console.log('Thursday: learn articles about JS');
+            break;
+        case 5:
+            console.log('Friday: practice JS');
+            break;
+        case 6:
+            console.log('Saturday: resting');
+            break;
+        case 7:
+            console.log('Sunday: meet with friends');
+            break;
+    }
+
+// ---------------------------------------2.5---------------------------------------
