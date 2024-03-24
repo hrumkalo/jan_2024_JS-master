31107c3055271062e0ec94f8140751a3ac2dcf7c
Ukranian.Falcon@gmail.com <Ukrainian.Falcon@gmail.com>
2024-03-24 17:15:55 +0200

    Homework 3/6

--- jan_2024_JS-master/Homeworks/HW_3/homework_3.js
+++ jan_2024_JS-master/Homeworks/HW_3/homework_3.js
@@ -45,26 +45,67 @@
 
 //------------------------------------------------------3/6------------------------------------------------------
 
-let products = [
-    {
-        title: 'milk',
-        price: 22,
-        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-    },
-    {
-        title: 'juice',
-        price: 27,
-        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-    },
-    {
-        title: 'tomato',
-        price: 47,
-        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-    },
-    {
-        title: 'tea',
-        price: 15,
-        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-    },
-];
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+//
+// for(const product of products){
+//     document.write(
+//         `<div class="productCard">
+//             <h3 class="productTitle">${product.title}, ${product.price}</h3>
+//             <img class="productImage" src ="${product.image}" alt="pic" >
+//         </div>`)
+// }
 
+//------------------------------------------------------3/7------------------------------------------------------
+
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+
+// for (let user of users) {
+//     if (user.age > 30){
+//     console.log(user.name, '-',  user.age);
+//     }
+// }
+
+// for (let user of users) {
+//     if (user.status === true){
+//         console.log(user.name);
+//     }
+// }
+
+// for (let user of users) {
+//     if (user.status !== true){
+//         console.log(user.name);
+//     }
+// }
