c33029de9367cf25b04b96a8f0b84c2b8907439c
Ukranian.Falcon@gmail.com <Ukrainian.Falcon@gmail.com>
2024-03-31 23:25:24 +0300

    Homework 4/5,6,7,8, 9

--- jan_2024_JS-master/Homeworks/HW_4/homework_4.js
+++ jan_2024_JS-master/Homeworks/HW_4/homework_4.js
@@ -24,3 +24,69 @@
 
 //------------------------------------------------------4/4------------------------------------------------------
 
+// let listOfItems = ['html', ' css', ' javascript', ' mysql', ' mongodb', ' react', ' angular', ' node.js'];
+// function list(array){
+//     for (const item of array);{
+//         document.write(item);
+//     }
+//
+// }
+// document.write(listOfItems);
+
+//------------------------------------------------------4/5------------------------------------------------------
+
+// function TextSection (text){
+//     document.write(`<p>${text}</p>`);
+// }
+// TextSection("Welcome to my page");
+
+//------------------------------------------------------4/6------------------------------------------------------
+
+// function OurText(text){
+//     document.write(`<ul>
+//         <li>${text}</li>
+//         <li>${text}</li>
+//         <li>${text}</li>
+// </ul>`)
+// }
+// OurText("Hello people");
+
+//------------------------------------------------------4/7------------------------------------------------------
+
+// function OurTask2(text, number){
+//     document.write(`<ul>`)
+//         for (let i = 0; i < number; i++){
+//             document.write(`<li>${text}</li>`)
+// }
+//     document.write(`</ul>`);
+// }
+// OurTask2('Hello people', 3);
+
+//------------------------------------------------------4/8------------------------------------------------------
+
+// let SomeList = [125, "ky-ky", true, false];
+// function data(array){
+//     document.write(`<ul>`)
+//     for (const item of array) {
+//         document.write(`<li>${item}</li>`)
+//     }
+//     document.write(`</ul>`)
+// }
+// data(SomeList);
+
+//------------------------------------------------------4/9------------------------------------------------------
+
+// let listOfPeople = [
+//     {id: 1, name: "Viki", age: 50},
+//     {id: 2, name: "Miki", age: 100},
+//     {id: 3, name: "Fiki", age: 10}
+// ]
+//
+// function obj(array){
+//     for (const item of array) {
+//         document.write(`<div>${item.id}. ${item.name}, ${item.age}</div>`)
+//     }
+// }
+// obj(listOfPeople);
+
+//------------------------------------------------------4/10------------------------------------------------------
